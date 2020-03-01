d730649e2b3bd52b0c46345e30224412ac90175c
Enrique Pittaluga <ep7m@me.com>
2020-03-01 18:35:47 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -3,7 +3,7 @@ require 'directors_database'
 require 'pp'
 # Call the method directors_database to retrieve the NDS
 
-directors_database()
+puts directors_database()
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
